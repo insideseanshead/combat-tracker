25501d68e30cd9e2a813f2e28db607953e1674ee
Sean Morgan <insideseanshead@gmail.com>
2021-02-16 21:49:52 -0800

    added header and navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Combat Tracker App</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Jumbotron, Button } from "react-bootstrap";
+import Navbar from './Navbar'
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar />
+      <Jumbotron className="jumbo">
+        <div className="header_text">
+          <h1>Hello, world!</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for
+            calling extra attention to featured content or information.
+          </p>
+          <p>
+            <Button variant="primary">Learn more</Button>
+          </p>
+        </div>
+      </Jumbotron>
+    </header>
+  );
+};
+
+export default Header;
