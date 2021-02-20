4e38543dd8c2bbf1a8019b556ec33f9d382a1670
Sean Morgan <insideseanshead@gmail.com>
2021-02-20 14:44:16 -0800

    created monster.js, animals.js, and npcs.js in routes folder and created get requests for monsters.js

---
+++ routes/monsters.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+let Monster = require('../models/monster.model');
+
+// Get all monsters
+router.route('/').get((req,res) => {
+    Monster.find()
+        .then((monsters) => res.json(monsters))
+        .catch((err) => res.status(400).json('Error: ' + err));
+})
+
+//Get monster by id
+router.route('/:id').get((req, res) => {
+    Monster.findById(req.params.id)
+        .then((monster) => res.json(monster))
+        .catch((err) => res.status(400).json("Error: " + err))
+})
