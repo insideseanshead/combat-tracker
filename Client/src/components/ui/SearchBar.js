f9e9a204b3b88abc2c7f5712af26748495bdaf74
Sean Morgan <insideseanshead@gmail.com>
2021-02-27 20:19:05 -0800

    set up state and added onchange event to form control for the search bar function of app

--- Client/src/components/ui/SearchBar.js
+++ Client/src/components/ui/SearchBar.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
 const SearchBar = () => {
+  const [text, setText] = useState('');
+
   return (
     <Form className="form-bg p-4">
       <div className="row">
         <div className="col-10">
           <Form.Group className="m-0" controlId="formBasicEmail">
-            <Form.Control type="search" placeholder="Search Beastiary" />
+            <Form.Control type="text" placeholder="Search Beastiary" value={text} onChange={(e)=> setText(e.target.value)} autofocus />
           </Form.Group>
         </div>
         <div className="col-2">
