4ddd724518a3ba45de2f50f2712680a335d82a48
Sean Morgan <insideseanshead@gmail.com>
2021-03-25 21:59:12 -0700

    Connected front end to heroku api

--- Client/src/components/utils/API.js
+++ Client/src/components/utils/API.js
@@ -1,3 +1,31 @@
+// const URL_PREFIX = "http://localhost:5000"
+const URL_PREFIX = "https://combattracker-api.herokuapp.com"
+
+const API = {
+    login: function (userData) {
+      console.log(userData);
+      return fetch("/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": `${URL_PREFIX}application/json`,
+        },
+        body:JSON.stringify(userData),
+      })
+        .then(res=> res.json())
+        .catch((err) => console.log(err));
+    },
+
+    getProfile: function(token){
+      return fetch(`${URL_PREFIX}/api/users/secretProfile`, {
+        headers:{
+          "authorization": `Bearer ${token}`
+        }
+      }).then(res=>res.json()).catch(err=>null)
+    }
+  };
+  
+  module.exports = API;
+
 // import axios from 'axios';
 
 // export default{
@@ -16,29 +44,3 @@
 //         return axios.get('/api/npcs')
 //     }
 // }
-
-const API = {
-    login: function (userData) {
-      console.log(userData);
-      return fetch("https://combattracker-api.herokuapp.com/api/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body:JSON.stringify(userData),
-      })
-        .then(res=> res.json())
-        .catch((err) => console.log(err));
-    },
-
-    getProfile: function(token){
-      return fetch("https://combattracker-api.herokuapp.com/api/users/secretProfile", {
-        headers:{
-          "authorization": `Bearer ${token}`
-        }
-      }).then(res=>res.json()).catch(err=>null)
-    }
-  };
-  
-  module.exports = API;
-
