ad660ca30c8f7b5c97ba1b6d3bc0a0bcff0d1bd6
Sean Morgan <insideseanshead@gmail.com>
2021-03-26 23:21:09 -0700

    comented out navbar for now as I don't have any other pages set up yet.

--- src/components/ui/Loading.js
+++ src/components/ui/Loading.js
@@ -3,7 +3,7 @@ import loading from '../../img/loading.gif'
 
 const Loading = () => {
     return (
-        <img src={loading} style={{width:'400px', margin: 'auto', display: 'block'}} alt='loading' />
+        <img src={loading} style={{width:'800px', margin: 'auto', display: 'block'}} alt='loading' />
     )
 }
 
