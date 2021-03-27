2c6454b82336e730a27d9792856ba5310e8d6eed
Sean Morgan <insideseanshead@gmail.com>
2021-03-26 23:17:21 -0700

    refactored login state back to landing component

--- src/App.js
+++ src/App.js
@@ -11,64 +11,7 @@ import API from "./components/utils/API";
 
 
 function App() {
-  // State for Login
-  const [loginFormState, setloginFormState] = useState({
-    email: "",
-    password: "",
-  });
 
-  const [profileState,setProfileState]=useState({
-    name:'',
-    email:'',
-    campaigns:[],
-    isLoggedIn:false
-  })
-
-  useEffect(()=>{
-    const token = localStorage.getItem('token');
-    API.getProfile(token).then(profileData=>{
-      if(profileData){
-        setProfileState({
-          name:profileData.name,
-          email:profileData.email,
-          campaigns:profileData.Campaigns,
-          isLoggedIn:true
-        })
-      } else {
-        localStorage.removeItem('token');
-        setProfileState({
-          name: "",
-          email: "",
-          tanks: [],
-          isLoggedIn: false,
-        })
-      }
-    })
-  },[])
-
-  const inputChange = (event) => {
-    const { name, value } = event.target;
-    setloginFormState({
-      ...loginFormState,
-      [name]: value,
-    });
-  };
-
-  const formSubmit = event=>{
-    event.preventDefault();
-    API.login(loginFormState).then(newToken=>{
-      localStorage.setItem('token',newToken.token)
-      API.getProfile(newToken.token).then(profileData=>{
-        setProfileState({
-          name:profileData.name,
-          email:profileData.email,
-          campaigns:profileData.Campaigns,
-          isLoggedIn:true
-        })
-        console.log(profileData)
-      })
-    })
-  } 
   // State for Beastiary Cards
   const [monsters, setMonsters] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -91,37 +34,6 @@ function App() {
       <div class="background">
       <Container className="container">
         <br />
-        
-        {/* <Row>
-        <Col md={{ span: 4, offset: 4 }}>
-          <Form onSubmit={formSubmit}>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Control
-                onChange={inputChange}
-                value={loginFormState.email}
-                type="text"
-                name="email"
-                placeholder="Enter email"
-              />
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Control
-                onChange={inputChange}
-                value={loginFormState.password}
-                type="password"
-                name="password"
-                placeholder="password"
-              />
-            </Form.Group>
-            <Button variant="primary" type="submit" value="login">
-              Submit
-            </Button>
-          </Form>
-          {profileState.isLoggedIn?profileState.campaigns.map(campaignObj=><p>{campaignObj.name}</p>):<h1>log in to see your campaigns</h1>}
-        </Col>
-      </Row> */}
-
-
         <SearchBar getQuery={(q) => setQuery(q)} />
         <div className="body-bg">
           <BestiaryGrid isLoading={isLoading} monsters={monsters} query={query}/>
--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -4,10 +4,40 @@ import API from "../utils/API";
 
 const Landing = () => {
   // State for Login
+  // State for Login
   const [loginFormState, setloginFormState] = useState({
     email: "",
     password: "",
   });
+
+  const [profileState,setProfileState]=useState({
+    name:'',
+    email:'',
+    campaigns:[],
+    isLoggedIn:false
+  })
+
+  useEffect(()=>{
+    const token = localStorage.getItem('token');
+    API.getProfile(token).then(profileData=>{
+      if(profileData){
+        setProfileState({
+          name:profileData.name,
+          email:profileData.email,
+          campaigns:profileData.Campaigns,
+          isLoggedIn:true
+        })
+      } else {
+        localStorage.removeItem('token');
+        setProfileState({
+          name: "",
+          email: "",
+          tanks: [],
+          isLoggedIn: false,
+        })
+      }
+    })
+  },[])
 
   const inputChange = (event) => {
     const { name, value } = event.target;
@@ -19,10 +49,19 @@ const Landing = () => {
 
   const formSubmit = event=>{
     event.preventDefault();
-    API.login(loginFormState).then(loginData=>{
-      console.log(loginData)
+    API.login(loginFormState).then(newToken=>{
+      localStorage.setItem('token',newToken.token)
+      API.getProfile(newToken.token).then(profileData=>{
+        setProfileState({
+          name:profileData.name,
+          email:profileData.email,
+          campaigns:profileData.Campaigns,
+          isLoggedIn:true
+        })
+        console.log(profileData)
+      })
     })
-  }
+  } 
 
   return (
     <div className="login">
