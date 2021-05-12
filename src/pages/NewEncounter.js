4c1917d816dada97e8d5ce91c92fe0d8c53e175a
Sean Morgan <insideseanshead@gmail.com>
2021-05-11 22:24:03 -0700

    layout create new encounter page

--- src/pages/NewEncounter.js
+++ src/pages/NewEncounter.js
@@ -1,13 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Form, Button, Row, Col } from "react-bootstrap";
-import API from "../components/utils/API"
+import API from "../components/utils/API";
 
 const NewEncounter = () => {
-    return (
-        <div>
-            <h1>Create New Encounter</h1>
-        </div>
-    )
-}
+  return (
+    <div className='NewEncounterPage'>
+      <Row className="NewEncounterHead">
+          <Col className="m-4">
+              <Form>
+                  <Form.Group>
+                      <Form.Control
+                        type='text'
+                        name='encounterName'
+                        placehoder='Encounter Name'
+                      />
+                  </Form.Group>
+                  <Button>
+                      Save Encounter
+                  </Button>
+              </Form>
+          </Col>
+      </Row>
+      <Row className='EncounterCreator'>
+          <Col className='EncounterMonsterList col-8'>
+              
+          </Col>
+          <Col className='SelectedMonsters col-4'>
+              
+          </Col>
+      </Row>
+    </div>
+  );
+};
 
-export default NewEncounter
+export default NewEncounter;
